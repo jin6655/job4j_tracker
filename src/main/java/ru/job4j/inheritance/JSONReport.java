3703174e0fbf05f119fdc79486d05482bf5db552
jin6655 <vasilypoiser@yandex.ru>
2022-05-08 12:41:38 +0300

    4. Переопределение

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,7 +1,8 @@
-package ru.job4j.tracker;
+package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + "\t\"name\" : " + "\"" + name + "\"," + System.lineSeparator()
