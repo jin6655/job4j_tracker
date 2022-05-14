19c65c786d0acf5b3dd9a68f441f9c5e64bea350
jin6655 <vasilypoiser@yandex.ru>
2022-05-14 17:12:05 +0300

    9. toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,8 +7,11 @@ public class StartUI extends Item {
 
     public static void main(String[] args) {
         Item dateTime = new Item();
+        Item objectPrint = new Item();
         DateTimeFormatter dTM = DateTimeFormatter.ofPattern("dd.MM.u HH:mm:ss EEEE ");
         String dTFormat = dateTime.getCreated().format(dTM);
         System.out.println("Текущие время и дата: " + dTFormat);
+        System.out.println(objectPrint);
     }
+
 }
