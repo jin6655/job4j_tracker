6c58e058260dd49536b35f9d2ce6159aa6b09cf6
jin6655 <vasilypoiser@yandex.ru>
2022-06-16 01:35:08 +0300

    1. Паспорт и жители

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void DoubleAdd() {
+        Citizen citizen01 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen01);
+        assertFalse(office.add(citizen));
+    }
+
+}
