b568da0748ead7662514dc9ca81334e02bd12afd
jin6655 <vasilypoiser@yandex.ru>
2022-06-16 00:50:11 +0300

    0. Map, HashMap

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> acc = new HashMap<>();
+        acc.put("trup@mail.ru", "Vasily");
+        acc.put("htr@mail.ru", "Genchik");
+        acc.put("ser@mail.ru", "Olga");
+        acc.put("ser@mail.ru", "Ivan");
+        acc.replace("trup@mail.ru", "ReplaceName");
+        for (String i : acc.keySet()) {
+            String j = acc.get(i);
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        for (Map.Entry<String, String> i : acc.entrySet()) {
+            String k = i.getKey();
+            String j = i.getValue();
+            System.out.println(k + " " + j);
+        }
+    }
+}
