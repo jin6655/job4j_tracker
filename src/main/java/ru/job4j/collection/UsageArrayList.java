a4375d48936ce5965dd986c796b1fe3dd04f9c72
jin6655 <vasilypoiser@yandex.ru>
2022-06-15 22:39:08 +0300

    0. Set, HashSet, Iterator.

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -3,22 +3,28 @@ package ru.job4j.collection;
 import ru.job4j.tracker.Item;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UsageArrayList {
 
     public static void main(String[] args) {
         List<Item> name = new ArrayList<>();
+        List<Integer> age = new ArrayList<>();
         Item item01 = new Item(2, "Petr");
         Item item02 = new Item(5, "Vasily");
         Item item03 = new Item(10, "Semka");
         name.add(item01);
         name.add(item02);
         name.add(item03);
-        name.remove(item03);
+        age.add(0, 5);
+        age.add(1, 5);
+        age.add(2, 89);
+        age.add(1, 55);
         for (Object value : name) {
             System.out.println(value);
         }
+        System.out.println(age);
     }
 
 }
---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<>();
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        for (Object i : autos) {
+            System.out.println(i);
+        }
+    }
+}
