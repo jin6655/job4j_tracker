ed655cafb3459112827d6d410a7b2329cda937c2
jin6655 <vasilypoiser@yandex.ru>
2022-07-17 22:48:03 +0300

    1. Фильтрация учеников

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+
+    public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        List<Student> rsl = new ArrayList<>();
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
+    }
+
+}
